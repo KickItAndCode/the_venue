2e9f14d3463bffc9afee3e5386c1f22f191a2a9f
rob henderson <robert.henderson@slalom.com>
2019-03-26 13:17:58 -0700

    Finished the discounts section

---
+++ src/components/Highlights/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Description from './Description';
+import Discount from '../header_footer/Discount';
+const Highlights = () => {
+    return (
+        <div className= "highlight_wrapper">
+            
+            <Description />
+            <Discount/>
+        </div>
+    );
+};
+
+export default Highlights;
---
+++ src/components/header_footer/Discount.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import Fade from 'react-reveal/Fade'
+import Slide from 'react-reveal/Fade'
+import MyButton from '../UI/MyButton'
+
+class Discount extends Component {
+    state = {
+        discountStart: 0,
+        discountEnd  : 30
+    }
+
+    percentage = () => {
+        if (this.state.discountStart < this.state.discountEnd){
+            this.setState({
+                discountStart:  this.state.discountStart +1
+            })
+        }
+    }
+    componentDidUpdate(){
+        setTimeout(() => {
+            this.percentage()
+        }, 30);
+    }
+    render() {
+        return (
+
+
+            <div className="center_wrapper">
+                <div className="discount_wrapper">
+                    <Fade
+                        onReveal={() => this.percentage()}
+                        >
+
+                        <div className="discount_porcentage">
+                            <span> {this.state.discountStart}% </span>
+                            <span> OFF</span>
+                        </div>
+                    </Fade>
+                    <Slide right>
+                        <div className="discount_description">
+                            <h3> Purchase Tickets Before March 31st</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                           
+                                <MyButton 
+                                    text="Purchase Tickets"
+                                    bck="#ffa800"
+                                    color="#ffffff"
+                                    link="http://google.com"
+                                    />
+                            
+                        </div>
+                    </Slide>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Discount;
